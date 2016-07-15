b42f60ff214cfdaf7a91b113ee59fab3c9ea859e
hotb0x <hotb0x@users.noreply.github.com>
2016-07-14 18:13:23 -0700

    Add files via upload

---
+++ sites/instastream/js/main.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function ($) {
+
+  $("#instastream").instastream({
+		instaToken: '3544965596.1677ed0.b26fb283e46344d88bc5af46bb523412',
+		instaUser: '3544965596',//
+		instaResults: 12,
+		instaMenu: 'yes'
+	});
+
+});
